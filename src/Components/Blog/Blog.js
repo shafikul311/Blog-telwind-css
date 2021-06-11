12b4e9347865684f6fdbadc31beb0c6bc7329286
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2021-06-11 16:10:44 +0600

    loadign add

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -5,6 +5,7 @@ import Blogs from './Blogs';
 const Blog = () => {
 
     const [ blog, setBlog ] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() =>{
         const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=893eb9a7287a485cbd24660331cd266d`
@@ -12,12 +13,20 @@ const Blog = () => {
         fetch(url)
         .then(res => res.json())
         .then(data => setBlog(data?.articles))
+        setIsLoading(false);
 
     },[])
     return (
         <div>
             {
-                blog?.map(blogs => <Blogs key={blog.author} blogs={blogs}></Blogs>)
+                isLoading && <p className="text-center text-red-800 text-5xl">loading</p>
+                
+              
+            }
+          
+            
+            {
+                blog?.map(blogs => <Blogs key={blogs.id} blogs={blogs}></Blogs>)
             }
         </div>
     );
